9a092c55ac774c48a69dd39ad4bebb48824b9e78
Polo1002 <polo.lahbabi@gmail.com>
2026-04-18 17:46:07 +0200

    Set base path for Vite configuration
    Update Vite configuration to set base path.

--- Site/vite.config.ts
+++ Site/vite.config.ts
@@ -6,9 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    base: '/KlassDirect/', // TRÈS IMPORTANT : le nom exact de ton dépôt
+  // ... le reste de ta config
     },
     resolve: {
       alias: {
